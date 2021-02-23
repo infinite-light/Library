82200d915792652b620bb963987da1b2b3b89518
devendra <devpatel1837@gmail.com>
2021-02-22 23:00:09 -0500

    testing

--- catalog/views.py
+++ catalog/views.py
@@ -57,7 +57,8 @@ class BookDetailView(generic.DetailView):
 
 class AuthorListView(generic.ListView):
     model = Author
-    paginate_by = 10
+
+    paginate_by = 2
     
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get the context
